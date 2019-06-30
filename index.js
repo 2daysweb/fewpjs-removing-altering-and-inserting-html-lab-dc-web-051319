352271dd3b3e94c17800f62251b81fbf722455d3
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-30 14:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 main.remove() 
 
 let newHeader = document.createElement("h1")
@@ -9,5 +10,20 @@ let header_text = document.createTextNode("YOUR-NAME is the champion")
 
 newHeader.appendChild(header_text)
 
+=======
+document.querySelector("main").remove() ;
+
+let header = document.getElementById("h1#header")
+
+let main = document.getElementByTag("main") 
+main.remove() 
+
+let newHeader = document.querySelector("body").childAppend
+
+
+var element = document.createElement("h1") 
+var victory_id = document.getElementById("victory")
+element.childAppend(newHeader)
+>>>>>>> 23d6ead5fc4ac3ccc84272f5bde5c47f86537c81
 
 
